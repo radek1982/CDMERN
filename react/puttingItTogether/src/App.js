34ea62735203d6201507a14685bfcd40c45e2156
Radek Wazny <radek.wazny@gmail.com>
2021-05-06 23:02:29 -0500

    bootstrapped puttingItTogether

---
+++ react/puttingItTogether/src/App.js
@@ -0,0 +1,16 @@
+
+function App() {
+  return (
+    <div> 
+      <h1> Hello Dojo</h1>
+      <p> Things I need to do: </p>
+      <ul> <li> Learn React</li>
+        <li>Climb Mt Everest</li>
+        <li> Run marathon</li>
+        <li> Feed the dogs</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
