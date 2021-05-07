53e6f4cefb5c776a21a7339fb1f95373c2c32f94
Radek Wazny <radek.wazny@gmail.com>
2021-05-06 20:35:29 -0500

    Hoisting assignment

---
+++ fundamentals/hoist.js
@@ -0,0 +1,97 @@
+// 1
+var hello = 'world';    
+console.log(hello);                                   
+
+// 2
+var needle = 'haystack';
+test();
+function test(){
+    var needle = 'magnet';
+    console.log(needle);
+}
+// 3 - print not called
+var brendan = 'super cool';
+function print(){
+    brendan = 'only okay';
+    console.log(brendan);
+}
+console.log(brendan);
+
+
+// 4
+
+var food = 'chicken';
+console.log(food);
+eat();
+function eat(){
+    food = 'half-chicken';
+    console.log(food);
+    var food = 'gone'; // var has no impact on global var
+}
+
+// 5 Error  mean not defined because of var
+
+mean();
+console.log(food);
+var mean = function() {
+    food = "chicken";
+    console.log(food);
+    var food = "fish";
+    console.log(food);
+}
+console.log(food);
+
+// 6 undefined (var prevents hoisting)
+//   rock
+//   r&b
+//   disco
+console.log(genre);
+var genre = "disco";
+rewind();
+function rewind() {
+    genre = "rock";
+    console.log(genre);
+    var genre = "r&b";
+    console.log(genre);
+}
+console.log(genre);
+
+// 7 san jose
+//   seatle
+//   burbank
+//   san jose
+
+dojo = "san jose";
+console.log(dojo);
+learn();
+function learn() {
+    dojo = "seattle";
+    console.log(dojo);
+    var dojo = "burbank";
+    console.log(dojo);
+}
+console.log(dojo);
+
+// 8
+/*
+{'name': Chicago,
+ 'students': 65,
+ 'hiring': true }
+
+error cannot assign to a constant
+*/
+
+console.log(makeDojo("Chicago", 65));
+console.log(makeDojo("Berkeley", 0));
+function makeDojo(name, students){
+    const dojo = {};
+    dojo.name = name;
+    dojo.students = students;
+    if(dojo.students > 50){
+        dojo.hiring = true;
+    }
+    else if(dojo.students <= 0){
+        dojo = "closed for now";
+    }
+    return dojo;
+}
