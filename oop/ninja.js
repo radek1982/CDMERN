864cdb5f83a33cb87380300c483a5f96b1fe1484
Radek Wazny <radek.wazny@gmail.com>
2021-05-06 21:27:04 -0500

    super ninja

--- oop/ninja.js
+++ oop/ninja.js
@@ -20,6 +20,28 @@ class Ninja {
     }
 }
 
+class Sensei extends Ninja{
+
+    constructor(name) {
+        super(name)
+        this.health = 200;
+        this.speed = 10;
+        this.strength =10;
+        this.wisdom = 10;
+    }
+
+    speakWisdom() {
+        this.drinkSake();
+        console.log('What one programmer can do in one month, two programmers can do in two months.');
+    }
+
+
+}
+
 const ninja1 = new Ninja("Hyabusa");
 ninja1.sayName();
 ninja1.showStats();
+
+const masterSplinter = new Sensei("Master Splinter");
+masterSplinter.speakWisdom();
+masterSplinter.showStats();
