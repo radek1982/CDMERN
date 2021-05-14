aabac952f6c76128f9ec5b7e89cb3ed6b90854df
Radek Wazny <radek.wazny@gmail.com>
2021-05-13 20:27:25 -0500

    hook form

---
+++ react/hookForm/src/App.js
@@ -0,0 +1,21 @@
+import React, {useState} from 'react';
+import Form from "./Form";
+import Data from "./Data";
+function App() {
+  const [state, setState] =  useState({
+    first: "",
+    last: "",
+    password: "", 
+    confirm: "",
+  })
+  return (
+    <div>
+      <Form state={state} update={setState}></Form>
+      <Data state={state}></Data>
+
+   </div>
+
+  )
+}
+
+export default App;
