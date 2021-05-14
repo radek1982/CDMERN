366e799ce21747c84da26bc95dec2a057265ab4d
Radek Wazny <radek.wazny@gmail.com>
2021-05-13 23:11:22 -0500

    More forms

--- react/hookForm/src/App.js
+++ react/hookForm/src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import Form from "./Form";
 import Data from "./Data";
 function App() {
+
   const [state, setState] =  useState({
     first: "",
     last: "",
@@ -9,9 +10,22 @@ function App() {
     password: "", 
     confirm: "",
   })
+  const minLen = (l) => (e) => e.length >=l;
+  const matches = (f) => (e,state) => {
+
+   return e ===state[f];
+  }
+
+  const [errors,setError] = useState({
+    first: {validation: minLen(2), msg: "First Name must be min 2 characters"},
+    last: {validation: minLen(2), msg: "Last Name must be min 2 characters"},
+    email: {validation: minLen(5), msg: "Email must be min 5 characters"},
+    password: {validation: minLen(8), msg:"Password must be min 8 characters"}, 
+    confirm: {validation: matches("password"), msg: "Confirmation must match password"},
+  })
   return (
     <div>
-      <Form state={state} update={setState}></Form>
+      <Form state={state} update={setState} errors={errors} setError={setError}></Form>
       <Data state={state}></Data>
 
    </div>
--- react/hookForm/src/Form.js
+++ react/hookForm/src/Form.js
@@ -1,26 +1,52 @@
 import React from 'react';
 
 const Form = props => {
-    const {state, update} = props;
+    const {state, update,errors, setError} = props;
     console.log(state);
 
-    
+const error = f =>{
+    return errors[f]["error"];
+}
 const change = e => {
+    const field = e.target.name;
+    const {validation, msg} = errors[field]
+
     update( {...state, [e.target.name]: e.target.value})
+    
+    if (!validation(e.target.value,state)) {
+
+        
+        setError({...errors, [field]: {validation: validation, 
+                                            msg: msg, "error": msg}}) 
+
+
+    } else {
+        setError({...errors, [field]: {validation: validation, msg:msg, "error": ""}}) 
+
+    }
+
+   
 };
     return (
         <form>
         <div>        
         <input  onChange={change} type="text" name="first" placeholder="First Name" />
+       <p> {error("first")}</p>
         </div>
 
-        <div><input  onChange={change} type="text" name="last"  placeholder="Last Name"/></div>
-        <div><input  onChange={change} type="text" name="email"  placeholder="Email"/></div>
-        <div>
-            
-            <input onChange={change}  type="password" name="password"  placeholder="Password"/>
+        <div><input  onChange={change} type="text" name="last"  placeholder="Last Name"/>
+        <p> {error("last")}</p>
         </div>
-        <div><input  onChange={change} type="password" name="confirm"   placeholder="Password Confirm"/>        </div>
+        <div><input  onChange={change} type="text" name="email"  placeholder="Email"/>
+        <p> {error("email")}</p>
+        </div>
+        <div>    
+        <input onChange={change}  type="password" name="password"  placeholder="Password"/>
+        <p> {error("password")}</p>
+        </div>
+        <div><input  onChange={change} type="password" name="confirm"   placeholder="Password Confirmation"/>       
+        <p> {error("confirm")}</p> 
+        </div>
 
         </form>
 
