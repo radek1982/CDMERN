93945718f98c7efb74916159271a5189eb4a5458
Radek Wazny <radek.wazny@gmail.com>
2021-05-18 04:13:11 -0500

    fixed server side validation

--- mern/product_manager/server/models/product.model.js
+++ mern/product_manager/server/models/product.model.js
@@ -3,10 +3,10 @@ const mongoose = require("mongoose");
 const ProductSchema = new mongoose.Schema({
 	title: {
 		 type: String,
-		 minlength: [3, "title must be at least 3 characters"],
+		 required: true,
 	},	 
 	price: {
-		type: String,
+		type: Number,
 		required: true
 	},
 	description: {
