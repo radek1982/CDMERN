8fc9b49e9fcdd81b3cb3785f5bde4a64306264d1
Radek Wazny <radek.wazny@gmail.com>
2021-05-18 02:08:55 -0500

    Added joke validations

--- mern/jokes/server/models/joke.model.js
+++ mern/jokes/server/models/joke.model.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose");
 
 const JokeSchema = new mongoose.Schema({
-	setup: String,
-	punchline: String
+	setup: {
+		 type: String,
+		 minlength: [10, "setup must be at least 10 characters"],
+	},	 
+	punchline: {
+		type: String,
+		minlength: [3, "punchline must be at least 3 characters"]
+	}
 });
 
 const Joke = mongoose.model("Joke", JokeSchema);
