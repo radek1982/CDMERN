bcf44dc74e88ef176ec07aa522c56d7cd74eac77
Radek Wazny <radek.wazny@gmail.com>
2021-05-17 23:25:15 -0500

    routing practice

---
+++ react/routing/src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./App.css"
+import {Router} from '@reach/router'
+
+const Output = (props) => {
+
+  let message = props.message || "welcome";
+  let bg = props.background || "#FFF";
+  let color = props.color || "#000";
+
+
+  if (!isNaN(message))  {
+    message = `The number is: ${message}`;
+  } else {
+    if (message!=="welcome")
+    message = `The word is: ${message}`
+  }
+
+  return (<div style={{background: bg, color: color}}>{message}</div>)
+}
+
+function App() {
+  return (<Router> 
+    <Output path="/"></Output>
+    <Output path=":message"></Output>
+    <Output path=":message/:background/:color"></Output>
+    
+ 
+  
+  </Router>)
+}
+
+export default App;
