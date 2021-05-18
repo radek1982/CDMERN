874a0660036a891da8656c6f0f8f4a148193b10e
Radek Wazny <radek.wazny@gmail.com>
2021-05-18 06:46:48 +0000

    changed boilerplate references

--- mern/jokes/server/controllers/joke.controller.js
+++ mern/jokes/server/controllers/joke.controller.js
@@ -2,7 +2,7 @@ const Joke = require("../models/joke.model");
 
 module.exports.findAll = (req, res) => {
   Joke.find()
-    .then(allDaUsers => res.json({ users: allDaUsers }))
+    .then(allJokes => res.json({ jokes: allJokes }))
     .catch(err => res.json({ message: "Something went wrong", error: err }));
 };
 
